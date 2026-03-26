607449a3644cc27b73d4e3de7a766534d7ad2c51
Mrv <Mrv@DESKTOP-I1PTCGD>
2026-03-26 19:55:30 +0100

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using CoreWCF;
+
+namespace CurrencyExchangeWCF
+{
+    [ServiceContract]
+    public interface IExchangeService
+    {
+        [OperationContract]
+        string GetMessage();
+    }
+
+    public class ExchangeService : IExchangeService
+    {
+        public string GetMessage()
+        {
+            return "Hello from WCF Service!";
+        }
+    }
+}
