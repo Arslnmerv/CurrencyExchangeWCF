7a8959d7de8bc8fec80ba2803a4fd29367cad57c
Arslnmerv <arslanmerve1619@gmail.com>
2026-04-15 12:02:02 +0200

    Add CurrencyExchangeClient and fix project structure

---
+++ CurrencyExchangeClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ServiceModel;
+
+[ServiceContract]
+public interface IExchangeService
+{
+    [OperationContract]
+    Task<decimal> GetExchangeRate(string currencyCode);
+
+    [OperationContract]
+    Task<decimal> BuyCurrency(string currencyCode, decimal amount);
+
+    [OperationContract]
+    Task<decimal> SellCurrency(string currencyCode, decimal amount);
+
+    [OperationContract]
+    Task<List<string>> GetAvailableCurrencies();
+}
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var binding = new BasicHttpBinding();
+        var endpoint = new EndpointAddress("http://localhost:58749/ExchangeService.svc");
+
+        var channelFactory = new ChannelFactory<IExchangeService>(binding, endpoint);
+        var client = channelFactory.CreateChannel();
+
+        var rate = await client.GetExchangeRate("USD");
+        Console.WriteLine($"USD rate: {rate}");
+
+        await ((IAsyncDisposable)client).DisposeAsync();
+    }
+}
